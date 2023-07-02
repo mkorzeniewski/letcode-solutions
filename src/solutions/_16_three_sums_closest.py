ceb70fc74685d0d129207dab061bf3d11618cf63
Michal Korzeniewski <michal.korzeniewski@reiba.com.cy>
2023-07-02 11:26:45 +0200

    Make it little faster

--- src/solutions/_16_three_sums_closest.py
+++ src/solutions/_16_three_sums_closest.py
@@ -6,15 +6,16 @@ class ThreeSumsClosest:
     @staticmethod
     def threeSum(nums: List[int], target: int) -> int:
         closest_difference = (sys.maxsize, 0)
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j+1, len(nums)):
-                    sum3 = nums[i] + nums[j] + nums[k]
-                    diff = sum3 - target
-                    if diff == 0:
-                        return sum3
-                    diff = - diff if (diff < 0) else diff
-                    if closest_difference[0] > diff:
-                        closest_difference = (diff, sum3)
-        return closest_difference[1]
+        for i in range(0, len(nums)-2):
+            two_sums = [nums[i] + nums[i+1] - target]
+            for j in range(i + 2, len(nums)):
+                for k in range(0, len(two_sums)):
+                    sum3 = nums[j] + two_sums[k]
+                    if sum3 == 0:
+                        return sum3 + target
+                    abs_sum3 = - sum3 if (sum3 < 0) else sum3
+                    if closest_difference[0] > abs_sum3:
+                        closest_difference = (abs_sum3, sum3 + target)
+                two_sums.append(nums[i] + nums[j] - target)
 
+        return closest_difference[1]
