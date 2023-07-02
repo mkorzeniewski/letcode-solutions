c871f531043749361a97a5d39ad7e3639807905c
Michal Korzeniewski <michal.korzeniewski@reiba.com.cy>
2023-07-02 13:13:12 +0200

    longest substring with repetitive characters using set

--- src/solutions/_3_longest_substring_without_repeating_chars.py
+++ src/solutions/_3_longest_substring_without_repeating_chars.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class LongestSubstring:
     @staticmethod
     def longest_substring(s: str) -> int:
@@ -8,6 +11,23 @@ class LongestSubstring:
             if s[i] in control_map and control_map[s[i]] >= low_position:
                 low_position = control_map[s[i]] + 1
             control_map[s[i]] = i
-            longest = longest if i+1 - low_position < longest else i+1 - low_position
+            longest = longest if i + 1 - low_position < longest else i + 1 - low_position
 
         return longest
+
+
+    @staticmethod
+    def longest_substring_set(s) -> int:
+        i, j, _max = 0, 0, 0
+        substr = set()
+
+        while j < len(s):
+            if s[j] not in substr:
+                substr.add(s[j])
+                j = j+1
+                _max = max(_max, len(substr))
+            else:
+                substr.remove(s[i])
+                i = i+1
+
+        return _max
--- src/test/test_3_longest_substring_without_repeating_chars.py
+++ src/test/test_3_longest_substring_without_repeating_chars.py
@@ -15,3 +15,8 @@ testdata = [
 @pytest.mark.parametrize("strs, expected", testdata)
 def test_longest_substring_without_repeating_chars(strs, expected):
     assert LongestSubstring.longest_substring(strs) == expected
+
+
+@pytest.mark.parametrize("strs, expected", testdata)
+def test_longest_substring_without_repeating_chars_set(strs, expected):
+    assert LongestSubstring.longest_substring_set(strs) == expected
